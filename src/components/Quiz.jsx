8d45792dba9b739931a8b62ae39493c5da98c42d
MarkBjj <markmullendev@gmail.com>
2026-09-17 14:34:02 -0300

    refactoring detremining the active question from active answer index

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 export default function Quiz() {
+  //Questions
+  // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // user Answers will also store which question was answered and what answer was selected
+  const [userAnswers, setUserAnswers] = useState([]);
+  // user active question index will be derived from the index of the answer that user has selected.
+  const activeQuestionIndex = userAnswers.length;
   return (
     <div>
       <h2>Quiz Questions</h2>
 
-      <p>This is where the quiz questions will be displayed.</p>
+      <p>Currently active question: {activeQuestionIndex + 1}</p>
     </div>
   );
 }
